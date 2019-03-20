e2306f1233cbf499fc3e4f77b72c120fd6bd3f04
Jonas Richiardi <richiard@stanford.edu>
2019-03-20 08:46:39 +0100

    bare minimum R code for TADPOLE leaderboard prediction; Mac OS python conda env for TADPOLE repo code

---
+++ prediction_models_jonas.R
@@ -0,0 +1,8 @@
+#' Returns NaNs for all quantities
+#'
+#' @param df_in A
+#' @param df_out A TADPOLE empty forecast dataframe for 84 months
+#' @return A TADPOLE prediction datafarme
+predict_nans <- function(df_in=NULL, df_out=NULL) {
+  stop('Not implemented yet')
+}
